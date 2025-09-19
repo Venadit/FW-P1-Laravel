a8c5bd5b8be044110af1e5784c2001371d8797a3
Venadit <Gideonvo.06@gmail.com>
2025-09-19 13:46:03 +0200

    Layouts Oefening

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -1,3 +1,18 @@
-<x-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+      <h1>Homepage</h1>
+        <a href="/About">About</a>
+        <a href="/Contact">Contact</a>
+        <a href="/Home"></a>
+    </nav>
 
-</x-layout>
+    <?php echo $slot; ?>
+</body>
+</html>
